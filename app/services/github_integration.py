4748d73df722aa9f85ccff8ddc9d9b685dcedb35
Krish Patel <patel.krish1003@gmail.com>
2024-12-20 16:14:56 +0530

    github content utils added

--- app/services/github_integration.py
+++ app/services/github_integration.py
@@ -3,6 +3,7 @@ from typing import List, Optional
 from github import Github
 from github.PullRequest import PullRequest
 from github.Repository import Repository
+from github.ContentFile import ContentFile
 import base64
 from urllib.parse import urlparse
 
@@ -42,13 +43,53 @@ class GithubService:
 
         return parts[0], parts[1]
 
-    
+ 
     def _get_repo(self, repo_url: str) -> Repository:
         owner, repo_name = self._parse_repo_url(repo_url=repo_url)
         return self.client.get_repo(f"{owner}/{repo_name}")
 
+
+    def _get_repo_content(self, repo_url: str):
+        repo = self._get_repo(repo_url=repo_url)
+        contents = repo.get_contents("")
+        return contents
+
+    def _build_tree_structure(
+        self, repo: Repository, contents: list[ContentFile] | ContentFile, tree_structure: str, file_paths: list,  level: int):
+
+        while contents:
+            file_content = contents.pop(0)
+            current_path = file_content.path
+
+            # Add proper indentation based on the level
+            indentation = "    " * level
+            if file_content.type == "dir":
+                tree_structure += f"{indentation}{file_content.name}/\n"
+                new_contents = repo.get_contents(file_content.path)
+                # Recursively call _traverse with an increased level
+                tree_structure = self._build_tree_structure(
+                    repo=repo, contents=new_contents, file_paths=file_paths, tree_structure=tree_structure, level=level + 1)
+            else:
+                tree_structure += f"{indentation}{file_content.name}\n"
+                file_paths.append(current_path)
+
+        return tree_structure
+
+    def get_tree_strucutre_and_file_paths(self, repo_url: str):
+        
+        repo = self._get_repo(repo_url=repo_url)
+        contents = self._get_repo_content(repo_url=repo_url)
+
+        tree_structure = ""
+        file_paths = []
+
+        tree_structure = self._build_tree_structure(repo=repo, contents=contents, tree_structure=tree_structure, file_paths=file_paths, level=0)
+        return tree_structure, file_paths
+
     
     def get_pr_details(self, repo_url: str, pr_number: int) -> PRDetails:
+
+        tree_str, file_paths = self.get_tree_strucutre_and_file_paths(repo_url=repo_url)
 
         try:
             repo = self._get_repo(repo_url=repo_url)
@@ -101,3 +142,12 @@ class GithubService:
         except Exception as e:
             print(f"Error fetching file content: {e}")
             return None
+
+
+# if __name__ == "__main__":
+
+#     client = GithubService("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")
+#     tree, file_paths = client.get_tree_strucutre_and_file_paths(repo_url="https://github.com/krish-patel1003/RMS")
+
+#     print("tree:\n", tree)
+#     print("file_paths:\n", file_paths)
